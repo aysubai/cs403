979533a3dac520ac0e3bcb8d7328af85d9a7794c
aysubai <aysubayramova2745@gmail.com>
2024-05-18 11:05:12 +0400

    feat: task1

---
+++ script.js
@@ -0,0 +1,90 @@
+//task1
+let multiply = 12 * 8
+console.log(multiply)
+
+let x = 10 / 2;
+console.log(x)
+
+let num1 = 20;
+let num2 = 17;
+let sum = num1 + num2;
+console.log(sum);
+
+let firstName = 'aysu'
+let lastName = 'Bayramova'
+let fatherName = 'Aliakber'
+let age = '20'
+let info = `name:${firstName} surname:${lastName} fathername:${fatherName} age:${age}`
+console.log(info);
+
+let f = 17 % 12;
+console.log(f);
+
+let cityName = 'Baki';
+cityName = 'Gence';
+console.log(cityName);
+
+//task2
+
+let a = 1, b = 1;
+let c = ++a;
+let d = b++;
+
+console.log(c);
+console.log(d);
+console.log(b);
+
+//task3
+
+let a1 = 2;
+let x1 = 1 + (a1 *= 2);
+console.log(x1);
+
+//task4
+
+console.log('test1', test);
+{
+    let test = "something"
+    console.log('test2', test);
+}
+console.log('test3', test);
+
+
+console.log('test1', test);
+{
+    var test = "something"
+    console.log('test2', test);
+}
+console.log('test3', test);
+
+
+//task5
+
+// let name = "Ulfat"
+// let surname = Zakirli
+// let year = 2000
+// year = "2000"
+// let city;
+// let qualification = null
+// let obj = { name: 'ulfat' }
+// let arr = ['a', 'b', 'c']
+
+let name = "Ulfat";
+let surname = 'Zakirli';
+let year = 2000;
+year = "2000";
+let city;
+let qualification = null;
+let obj = { name: 'ulfat' };
+let arr = ['a', 'b', 'c'];
+
+// console.log(typeof name , surname , year , year , city ,qualification , obj , arr);
+
+console.log(typeof name);          
+console.log(typeof surname);       
+console.log(typeof year);          
+console.log(typeof city);         
+console.log(typeof qualification); 
+console.log(typeof obj);          
+console.log(typeof arr);  
+
